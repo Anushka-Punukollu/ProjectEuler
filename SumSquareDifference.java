0ab57b5247a8d298771dde548a320968a711ae8d
Anushka Punukollu <121831894+Anushka-Punukollu@users.noreply.github.com>
2023-05-01 10:03:14 -0400

    Create SumSquareDifference.java

---
+++ SumSquareDifference.java
@@ -0,0 +1,11 @@
+
+public class SumSquareDifference {
+    public static void main(String[] args) 
+    {
+        int n = 100;
+        int sumOfSquares = n * (n + 1) * (2 * n + 1) / 6;
+        int squareOfSum = (int) Math.pow(n * (n + 1) / 2, 2);
+        int difference = squareOfSum - sumOfSquares;
+        System.out.println(difference);
+    }
+}
